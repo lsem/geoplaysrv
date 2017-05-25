e595ce5ee7d457cc16fcd78f763a34ae23338c3e
Liubomyr Semkiv <lyubomyr.semkiv@hotmail.com>
2017-05-25 12:05:43 +0300

    Refactored to not use dot-import

--- handlers/ApproxRect.go
+++ handlers/ApproxRect.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	. "github.com/lsem/geosrv/helpers"
+	hlp "github.com/lsem/geosrv/helpers"
 	"github.com/lsem/geosrv/viewModels"
 
 	"github.com/golang/geo/s2"
@@ -21,44 +21,44 @@ func ApproxRect(w http.ResponseWriter, r *http.Request) {
 	maxLvl := r.URL.Query().Get("maxLvl")
 	maxCells := r.URL.Query().Get("maxCells")
 
-	if len(minLvl) == 0 || !IsInt(minLvl) {
+	if len(minLvl) == 0 || !hlp.IsInt(minLvl) {
 		http.Error(w, "minLvl missing or bad", 400)
 		return
 	}
-	if len(maxLvl) == 0 || !IsInt(maxLvl) {
+	if len(maxLvl) == 0 || !hlp.IsInt(maxLvl) {
 		http.Error(w, "maxLvl missing or bad", 400)
 		return
 	}
-	if len(maxCells) == 0 || !IsInt(maxCells) {
+	if len(maxCells) == 0 || !hlp.IsInt(maxCells) {
 		http.Error(w, "maxCells missing or bad", 400)
 		return
 	}
-	if len(south) == 0 || !IsFloat(south) {
+	if len(south) == 0 || !hlp.IsFloat(south) {
 		http.Error(w, "south missing", 400)
 		return
 	}
-	if len(west) == 0 || !IsFloat(west) {
+	if len(west) == 0 || !hlp.IsFloat(west) {
 		http.Error(w, "west missing", 400)
 		return
 	}
-	if len(north) == 0 || !IsFloat(north) {
+	if len(north) == 0 || !hlp.IsFloat(north) {
 		http.Error(w, "north missing", 400)
 		return
 	}
-	if len(east) == 0 || !IsFloat(east) {
+	if len(east) == 0 || !hlp.IsFloat(east) {
 		http.Error(w, "east missing", 400)
 		return
 	}
 
-	rect := s2.RectFromLatLng(s2.LatLngFromDegrees(AsFloat(south), AsFloat(west)))
-	rect = rect.AddPoint(s2.LatLngFromDegrees(AsFloat(north), AsFloat(east)))
+	rect := s2.RectFromLatLng(s2.LatLngFromDegrees(hlp.AsFloat(south), hlp.AsFloat(west)))
+	rect = rect.AddPoint(s2.LatLngFromDegrees(hlp.AsFloat(north), hlp.AsFloat(east)))
 	fmt.Println("rect: ", rect)
 
 	region := s2.Region(rect)
 	fmt.Println("region: ", region)
 
-	rc := s2.RegionCoverer{MinLevel: AsInt(minLvl), MaxLevel: AsInt(maxLvl),
-		LevelMod: 0, MaxCells: AsInt(maxCells)}
+	rc := s2.RegionCoverer{MinLevel: hlp.AsInt(minLvl), MaxLevel: hlp.AsInt(maxLvl),
+		LevelMod: 0, MaxCells: hlp.AsInt(maxCells)}
 	covering := rc.Covering(region)
 
 	fmt.Println("covering: ", covering)
